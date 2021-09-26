721daaadb90e1527af275c345f13abf953f442ce
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-09-26 20:11:56 +0700

    update a bit

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,47 @@ import {
 
 const router = express.Router();
 
-const users = [];
+const users = [{
+    "type": "makanan",
+    "name": "nasi goreng",
+    "ingredients": [
+        "nasi", "cabai", "bawang putih", "bawang merah", "telur ayam"
+    ],
+    "price": 12000,
+    "stock": 24
+}, {
+    "type": "makanan",
+    "name": "nasi bakar",
+    "ingredients": [
+        "nasi", "cabai", "bawang putih", "daun pisang", "ayam suir"
+    ],
+    "price": 15000,
+    "stock": 12
+}, , {
+    "type": "makanan",
+    "name": "nasi lemak",
+    "ingredients": [
+        "nasi", "cabai", "bawang putih", "daging sapi", "santan"
+    ],
+    "price": 12000,
+    "stock": 52
+}, {
+    "type": "minuman",
+    "name": "es teh manis",
+    "ingredients": [
+        "air", "teh", "gula pasir"
+    ],
+    "price": 4000,
+    "stock": 12
+}, {
+    "type": "minuman",
+    "name": "es jeruk",
+    "ingredients": [
+        "air", "jeruk limau", "gula pasir"
+    ],
+    "price": 8000,
+    "stock": 18
+}];
 
 // starting with users
 router.get('/', (req, res) => {
