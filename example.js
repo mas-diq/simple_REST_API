8dfadb9e96db2871f48c8b3121e382e2b49e1e35
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-09-26 21:39:46 +0700

    change stucture

---
+++ example.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRoutes from './index.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.use('/users', userRoutes);
+
+// app.get('/', (req, res) => res.send("Hello from hompage"));
+app.get('/users', userRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port : http://localhost:${PORT}`));
--- index.js
+++ index.js
@@ -1,15 +1,68 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import userRoutes from './routes/users.js';
+// import express from 'express';
+// import {
+//     v4 as uuidv4
+// } from 'uuid';
 
-const app = express();
-const PORT = 5000;
+// const router = express.Router();
 
-app.use(bodyParser.json());
+const users = [{
+    "type": "makanan",
+    "name": "nasi goreng",
+    "ingredients": [
+        "nasi", "cabai", "bawang putih", "bawang merah", "telur ayam"
+    ],
+    "price": 12000,
+    "stock": 24
+}, {
+    "type": "makanan",
+    "name": "nasi bakar",
+    "ingredients": [
+        "nasi", "cabai", "bawang putih", "daun pisang", "ayam suir"
+    ],
+    "price": 15000,
+    "stock": 12
+}, , {
+    "type": "makanan",
+    "name": "nasi lemak",
+    "ingredients": [
+        "nasi", "cabai", "bawang putih", "daging sapi", "santan"
+    ],
+    "price": 12000,
+    "stock": 52
+}, {
+    "type": "minuman",
+    "name": "es teh manis",
+    "ingredients": [
+        "air", "teh", "gula pasir"
+    ],
+    "price": 4000,
+    "stock": 12
+}, {
+    "type": "minuman",
+    "name": "es jeruk",
+    "ingredients": [
+        "air", "jeruk limau", "gula pasir"
+    ],
+    "price": 8000,
+    "stock": 18
+}];
 
-app.use('/users', userRoutes);
+// // starting with users
+// router.get('/', (req, res) => {
+//     res.send(users);
+// });
 
-// app.get('/', (req, res) => res.send("Hello from hompage"));
-app.get('/users', userRoutes);
+// router.post('/', (req, res) => {
+//     const user = req.body;
+//     users.push({
+//         ...user,
+//         id: uuidv4()
+//     })
+//     res.send(`User with the name ${user.firstName} added to the databse!`);
+// });
 
-app.listen(PORT, () => console.log(`Server running on port : http://localhost:${PORT}`));
+// router.get('/:id', (req, res) => {
+//     res.send('The get id route');
+// })
+
+// export default router;
